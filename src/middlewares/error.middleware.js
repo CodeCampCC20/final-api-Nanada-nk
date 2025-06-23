c2fcaea881e68f57922cea2d356281383b18827f
Nanada-nk <nada.nidchakul@gmail.com>
2025-06-24 01:11:19 +0700

    fix bug

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,15 @@
-const errorMiddleware = (err, req, res, next) => {
-  const {statusCode = 500, message = "server error", field} = err
-  res.status(statusCode).json({success: false, message, ...err(field && {field})})
-}
+// const errorMiddleware = (err, req, res, next) => {
+//   const {statusCode = 500, message = "server error", field} = err
+//   res.status(statusCode).json({success: false, message, ...err(field && {field})})
+// }
 
-export default errorMiddleware
+// export default errorMiddleware
+
+const errorMiddleware = (err, req, res, next) => {
+  const { statusCode = 500, message = "server error", field } = err;
+  const errorResponse = { success: false, message };
+  if (field) errorResponse.field = field;
+  res.status(statusCode).json(errorResponse);
+};
+
+export default errorMiddleware;
